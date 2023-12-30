e30a04414bb0b4170696e126bb2b5cce450780b8
streq <strequena@gmail.com>
2023-12-30 18:18:47 -0300

    I'm trying to refactor ray re emissions

--- src/Camera2D.gd
+++ src/Camera2D.gd
@@ -1,1 +1,10 @@
 extends Camera2D
+
+func change_visible():
+	for child in get_children():
+		if "visible" in child:
+			child.visible = visible
+
+func _ready() -> void:
+	connect("visibility_changed", self, "change_visible")
+	change_visible()
