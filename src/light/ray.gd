2250c68f91e951d748865bf6a3f3bff8657d1d2f
streq <strequena@gmail.com>
2023-12-31 17:22:36 -0300

    nose

--- src/light/ray.gd
+++ src/light/ray.gd
@@ -3,12 +3,15 @@ class_name Ray
 
 signal collision(point, normal, color)
 signal no_collision(point)
-
+signal casted()
 
 onready var display = $"%display"
 var active = true
-
 var collision_point = Vector2()
+
+#copy of this ray recasted through a surface like a portal o a mirror
+var recast : Ray = null
+
 
 func check_colliding():
 	return active and is_colliding()
@@ -17,6 +20,13 @@ func _physics_process(delta):
 	update_ray()
 
 func update_ray():
+	_update_ray_self_only()
+	var current_recast = recast
+	while is_instance_valid(current_recast):
+		current_recast._update_ray()
+		current_recast = current_recast.recast
+
+func _update_ray_self_only():
 	active = true
 	force_raycast_update()
 	if is_colliding():
@@ -30,6 +40,4 @@ func update_ray():
 		emit_signal("collision", to_local(get_collision_point()), get_collision_normal(), color)
 	else:
 		emit_signal("no_collision", cast_to)
-
-	
 	
